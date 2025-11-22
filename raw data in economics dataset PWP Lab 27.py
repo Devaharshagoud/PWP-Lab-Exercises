f3f0d35c1d1585d87f314dbfa64d1ab40c50d753
Devaharshagoud <devaharshagoud@gmail.com>
2025-11-22 19:06:24 +0530

    Add files via upload

---
+++ raw data in economics dataset PWP Lab 27.py
@@ -0,0 +1,12 @@
+from plotnine.data import economics
+print(economics)
+
+
+from plotnine.data import economics
+from plotnine import ggplot, aes, geom_line
+
+(
+    ggplot(economics)  # What data to use
+    + aes(x="date", y="pop")  # What variable to use
+    + geom_line()  # Geometric object to use for drawing
+)
